c1afb9a94aab0465bd0283fc7409e48ca7a40a13
Peter Sharpe <psharpe@nvidia.com>
2025-09-26 21:31:37 -0400

    fighter

---
+++ naominagata/src/fighter.rs
@@ -0,0 +1,16 @@
+// Tutorial: Guns
+// Destroy the asteroid.
+use oort_api::prelude::*;
+
+pub struct Ship {}
+
+impl Ship {
+    pub fn new() -> Ship {
+        Ship {}
+    }
+
+    // Uncomment me, then press Ctrl-Enter (Cmd-Enter on Mac) to upload the code.
+    pub fn tick(&mut self) {
+        // fire(0);
+    }
+}
